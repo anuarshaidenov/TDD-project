d103dd31539ef5a917d22147af5deb99c1e71725
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-17 20:30:47 +0600

    Add test case 1 for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -2,4 +2,11 @@ class Solver
   def factorial(n)
     n == 1 || n == 0 ? 1 : factorial(n - 1) * n
   end
+  def reverse(word)
+    word = word.reverse
+  end
+
+  def fizzbuzz(n)
+    'fizz'
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../solver'
 
 describe Solver do
+  solver = Solver.new
+
   context "Testing factorial method" do
-    solver = Solver.new
 
     it "returns 1 if the input is 0 or 1" do
       expect(solver.factorial(0)).to eq 1
@@ -11,6 +12,31 @@ describe Solver do
 
     it "returns n factorial for given n" do
       expect(solver.factorial(5)).to eq 120
+    end
+  end
+
+  context "Testing reverse method" do
+
+    it "revers the input string, where the string is a single character" do
+      expect(solver.reverse('w')).to eq 'w'
+    end
+
+    it "revers the input string, where sting is multiple characters" do
+      expect(solver.reverse('Hello')).to eq 'olleH'
+    end
+
+    it "revers the input string, where sting is multiple words" do
+      expect(solver.reverse('Hello world')).to eq 'dlrow olleH'
+    end
+
+    it "revers the input string, where characters are digits" do
+      expect(solver.reverse('123456')).to eq '654321'
+    end
+  end
+
+  context "Testing fizzbuzz method" do
+    it "returns fizz when n is divisible by 3" do
+      expect(solver.fizzbuzz(3)).to eq 'fizz'
     end
   end
 end
